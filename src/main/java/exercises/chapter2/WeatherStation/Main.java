8ba33119408cb2fe7ad79d7d6afe0cc99453259c
Elizabeth Berg <elizabethinposti@gmail.com>
2018-11-05 22:25:54 +0200

    Add client code

--- src/main/java/exercises/chapter2/WeatherStation/Main.java
+++ src/main/java/exercises/chapter2/WeatherStation/Main.java
@@ -1,5 +1,19 @@
 package exercises.chapter2.WeatherStation;
 
-public class Main {
+import exercises.chapter2.WeatherStation.Displays.CurrentConditions;
+import exercises.chapter2.WeatherStation.Displays.ForecastDisplay;
+import exercises.chapter2.WeatherStation.Displays.StatisticsDisplay;
 
+public class Main {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        // Add new display observers
+        weatherData.registerObserver(new CurrentConditions());
+        weatherData.registerObserver(new ForecastDisplay());
+        weatherData.registerObserver(new StatisticsDisplay());
+
+        // Notify the observers for updated measurements
+        weatherData.notifyObservers();
+    }
 }
