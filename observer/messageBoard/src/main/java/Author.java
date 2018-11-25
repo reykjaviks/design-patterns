25c3e269d66f8110244d6f697c462b9df226c266
Elizabeth Berg <elizabethinposti@gmail.com>
2018-11-25 22:52:51 +0200

    Implement Subject methods

--- observer/messageBoard/src/main/java/Author.java
+++ observer/messageBoard/src/main/java/Author.java
@@ -1,16 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Author implements Subject {
     private String name;
-    private Message feed;
+    private List messageList;
+    private List observerList;
+
+    public  Author(String name) {
+        this.name = name;
+        messageList = new ArrayList<Message>();
+        observerList = new ArrayList<Observer>();
+    }
 
     @Override
     public void registerFollower(Observer o) {
+        this.observerList.add(o);
     }
 
     @Override
     public void removeFollower(Observer o) {
+        this.observerList.remove(o);
     }
 
     @Override
     public void notifyFollowers() {
+        for (Observer o : this.observerList) {
+            o.update();
+        }
     }
 }
