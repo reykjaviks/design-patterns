9ea99fa7873be1e2cd21cd4fdcc11cea7e6e59ff
Elizabeth Berg <elizabethinposti@gmail.com>
2018-12-01 18:23:05 +0200

    Perform software testing

--- observer/messageBoard/src/main/java/Main.java
+++ observer/messageBoard/src/main/java/Main.java
@@ -1,7 +1,21 @@
-
+import domain.Author;
+import domain.Follower;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Message Board");
+        Author eeCummings = new Author("E.E. Cummings");
+
+        Follower elizaditty = new Follower("elizaditty");
+        Follower kocu = new Follower("Kocu");
+        Follower sindex = new Follower("Sindex");
+
+        // elizaditty, kocu and sindex want to start following E.E. Cummings' posts.
+        eeCummings.registerObserver(elizaditty);
+        eeCummings.registerObserver(kocu);
+        eeCummings.registerObserver(sindex);
+
+        eeCummings.write("One's not half of two; two are halves of one.");
+        eeCummings.notifyObservers();
+
     }
 }
